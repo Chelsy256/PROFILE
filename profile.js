87f4fea98ecd65ee1ac83ec1a982c4e7e5379d5c
Sylvia Chelangat Mwanga <sylviachelsy@gmail.com>
2025-09-12 01:50:13 +0300

    my profile

---
+++ profile.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded"),
+( => {
+    const discoverBtn = getElementById("discover");
+    discoverBtn.addEventListener("Click",()=>{window.location.href = "services.html";});
+});
+
+
