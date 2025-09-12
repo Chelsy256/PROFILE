496110a48a355863016b1957dcf3a2bd56a89571
Sylvia Chelangat Mwanga <sylviachelsy@gmail.com>
2025-09-12 11:35:10 +0300

    my profile

--- profile.js
+++ profile.js
@@ -4,4 +4,11 @@ document.addEventListener("DOMContentLoaded"),
     discoverBtn.addEventListener("Click",()=>{window.location.href = "services.html";});
 });
 
-
+// Click on logo shows a welcome alert
+    const logo = document.getElementById("logo");
+    if (logo) {
+        logo.addEventListener("click", function (e) {
+            e.preventDefault(); // stop image from opening
+            alert("Welcome to Smart Nutrition UG!");
+        });
+    }
